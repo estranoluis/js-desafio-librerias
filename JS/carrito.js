8519c5855d88ca4dc126ee4cacd9392e1e200e24
Luis Estrano <estranoluis11866@gmail.com>
2022-09-24 12:34:07 -0300

    Desafio incorporando librerias

---
+++ JS/carrito.js
@@ -0,0 +1,112 @@
+// Variable carrito para que detecte si existen valores en el storage
+let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
+// Variables de monto y cantidad asi se mantienen los datos al refrescar la ventana
+let montoTotalCompra = calcularTotalCarrito();
+let cantidadTotalCompra = carrito.length;
+// Document ready con todo el codigo generado por el DOM
+$(document).ready(function () {
+  // Seccion carrito a traves jquery y dom
+  $("#section-carrito").append(`<div> 
+                              <h2>Total: $</h2> 
+                              <h2 id="montoTotalCompra">${montoTotalCompra}</h2>
+                              </div>
+                              <div> 
+                              <h3>Cantidad de productos:</h3> 
+                              <h3 id="cantidadTotalCompra">${cantidadTotalCompra}</h3>
+                              </div>
+                              <button class="boton" id="btn-finalizar">Finalizar compra</button>`);
+  // Boton finalizar compra (como confirmacion)
+  $("#btn-finalizar").on('click', function () {
+    const precioFinal = $("#montoTotalCompra").text();
+    // Sweet alert para confirmar y tambien vacia el carrito
+    Swal.fire({
+      title: '¿Seguro que queres finalizar tu compra?',
+      text: `Total a abonar: $${precioFinal}`,
+      showCancelButton: true,
+      confirmButtonColor: '#008f39',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si',
+      cancelButtonText: 'No'
+    }).then((result) => {
+      result.isConfirmed && Swal.fire(
+        'Compra confirmada',
+        '¡Que lo disfrutes!',
+        'success'
+        )
+      vaciarCarrito();
+      }
+    )}
+  );
+  //Se agrega selector para ordenar productos
+  $("#seleccion option[value='pordefecto']").attr("selected", true);
+  $("#seleccion").on("change", ordenarProductos);
+  // Se llama la funcion para crear las cards
+  productCards();
+})
+
+function productCards() {
+  for (const producto of productos) {
+    $("#section-productos").append(`<div class="card-product"> 
+                            <div class="img-container">
+                            <img src="${producto.foto}" alt="${producto.nombre}" class="img-product"/>
+                            </div>
+                            <div class="info-producto">
+                            <p class="font">${producto.nombre}</p>
+                            <p class="font">${producto.descripcion}</p>
+                            <strong class="font">$${producto.precio}</strong>
+                            <button class="botones" id="btn${producto.id}"> Agregar al carrito </button>
+                            </div>
+                            </div>`)
+    $(`#btn${producto.id}`).on('click', function () {
+    agregarAlCarrito(`${producto.id}`)
+    });
+  }
+};
+
+// Funcion que ordena productos segun precio, orden alfabetico y por defecto 
+function ordenarProductos() {
+  let seleccion = $("#seleccion").val();
+    if (seleccion == "defecto") {
+    productos.sort(function (a, b) {
+        return a.id - b.id
+    });
+    } else if (seleccion == "menor") {
+    productos.sort(function (a, b) {
+        return a.precio - b.precio
+    });
+    } else if (seleccion == "mayor") {
+    productos.sort(function (a, b) {
+        return b.precio - a.precio
+    });
+    } else if (seleccion == "alfabetico") {
+    productos.sort(function (a, b) {
+        return a.nombre.localeCompare(b.nombre);
+    });
+    }
+    // Se llama de nuevo la funcion luego de ordenar
+    $(".card-product").remove();
+    productCards();
+  }
+
+function agregarAlCarrito(id) {
+  carrito.push(productos.find(p => p.id == id));
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+  calcularTotalCarrito();
+}
+
+function calcularTotalCarrito() {
+  let total = 0;
+  for (const producto of carrito) {
+      total += producto.precio;
+  }
+  $("#montoTotalCompra").text(total);
+  $("#cantidadTotalCompra").text(carrito.length);
+  return total;
+}
+
+function vaciarCarrito() {
+  $("#montoTotalCompra").text("0");
+  $("#cantidadTotalCompra").text("0");
+  localStorage.clear();
+  carrito = [];
+}
---
+++ JS/productos.js
@@ -0,0 +1,11 @@
+const productos = [
+    {id:01, nombre:"Iphone 13", descripcion: "256 GB", precio: 1000000, foto: "./media/iphone13.jpg"},
+    {id:02, nombre:"Samsung Galaxy S22", descripcion: "128 GB", precio: 800000, foto: "./media/gs22.jpg"},
+    {id:03, nombre:"Xiaomi 12", descripcion: "128 GB", precio: 700000, foto: "./media/xiaomi12.jpg"},
+    {id:04, nombre:"Computador Ryzen 7", descripcion:"HP", precio: 1200000, foto: "./media/pcryzen7.jpg"},
+    {id:05, nombre:"Laptop i7", descripcion: "Asus", precio: 2000000, foto: "./media/asusi7.jpg"},
+    {id:06, nombre:"Laptop i5", descripcion: "Acer", precio: 2100000, foto: "./media/aceri5.jpg"},
+    {id:07, nombre:"Audifonos", descripcion: "Hyper X", precio: 100000, foto: "./media/auhyperx.jpg"},
+    {id:08, nombre:"Mouse", descripcion:"Razer", precio: 80000, foto: "./media/mouserazer.jpg"},
+    {id:09, nombre:"Case", descripcion:"Original", precio: 30000, foto: "./media/caseiphone.jpg"},
+];
